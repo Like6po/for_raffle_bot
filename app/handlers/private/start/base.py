87ca71e15389e4e71582d22d08308f023503e1c7
Like6po <begemot122122@gmail.com>
2022-07-20 19:02:54 +0300

    Улучшение: добавил пример использования контекста, прокинутого из мидлваря

--- app/handlers/private/start/base.py
+++ app/handlers/private/start/base.py
@@ -1,8 +1,12 @@
 from aiogram.types import Message
 
+from database.contexts.user import UserContext
 from keyboards.start import start_kb
 
 
-async def command_start(message: Message):
-    await message.answer(text=f'Привет, мир!',
+async def command_start(message: Message,
+                        user_db: UserContext):
+    user = await user_db.get_or_create_and_get(message.from_user)
+
+    await message.answer(text=f'Привет, {user.full_name}! Ваш id = {user.id}',
                          reply_markup=start_kb())
