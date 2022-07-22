3ab2ebeda7fdc7de2e066f5a1b3dc62526e2a24a
redbad <roma2006.romanov@yandex.ru>
2022-07-22 15:41:06 +0400

    Фикс: убрал лишние символы из модели канала

--- app/database/models/channel.py
+++ app/database/models/channel.py
@@ -5,6 +5,6 @@ from database import DatabaseModel
 
 class Channel(DatabaseModel):
     id = Column(Integer(), autoincrement=True, primary_key=True)
-    tg_id = Column(BigInteger(), unique=True, nullable=False)Eke
+    tg_id = Column(BigInteger(), unique=True, nullable=False)
     username = Column(VARCHAR(254), nullable=True, server_default=None)
     title = Column(Text(), nullable=False)
