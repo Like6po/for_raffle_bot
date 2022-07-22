042aec9ca76b8e2abde379ca9cd8b66b37a511b6
redbad <roma2006.romanov@yandex.ru>
2022-07-22 15:47:33 +0400

    Улучшение: добавил занесение нового канала в бд при вызове события my_chat_member

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -6,4 +6,4 @@ from .private import create_private_router
 
 def register_routers(dp: Dispatcher, session_pool, bot: Bot):
     dp.include_router(create_private_router(session_pool, bot))
-    dp.include_router(create_group_router())
+    dp.include_router(create_group_router(session_pool, bot))
--- app/handlers/group/__init__.py
+++ app/handlers/group/__init__.py
@@ -1,9 +1,15 @@
-from aiogram import Router
+from aiogram import Router, Bot
+
+from filters.chat_type import ChannelChatFilter
+from middlewares.init_contexts import InitMiddleware
+from .new_group.base import new_group
 
 
-def create_group_router() -> Router:
+def create_group_router(session_pool, bot: Bot) -> Router:
     group_router: Router = Router(name="group_router")
 
-    # Сообщения
+    group_router.my_chat_member.bind_filter(bound_filter=ChannelChatFilter)
+    group_router.my_chat_member.middleware(InitMiddleware(session_pool, bot))
+    group_router.my_chat_member.register(new_group)
 
     return group_router
