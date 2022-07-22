66e2f74b06b17f5a84d5123b221d21a969710a1b
Roma4ka-RedBad <cool.roma2006@gmail.com>
2022-07-22 21:56:26 +0400

    Улучшение: создал модель пользователей

---
+++ app/database/models/member.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, BigInteger, VARCHAR
+
+from database import DatabaseModel
+
+
+class Member(DatabaseModel):
+    id = Column(Integer(), autoincrement=True, primary_key=True)
+    tg_id = Column(BigInteger(), unique=True, nullable=False)
+    full_name = Column(VARCHAR(254), nullable=False)
+    username = Column(VARCHAR(254), nullable=True, server_default=None)
