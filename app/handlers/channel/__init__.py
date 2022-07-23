f5497adb160b1dd4d7cdb230dfbd7013ad167edb
Like6po <begemot122122@gmail.com>
2022-07-23 19:03:51 +0300

    Улучшение: пример регистрации хендлера колбека

--- app/handlers/channel/__init__.py
+++ app/handlers/channel/__init__.py
@@ -1,6 +1,7 @@
 from aiogram import Router, Bot
 
 from filters.chat_type import ChannelChatFilter
+from handlers.channel.contest_callback.base import contest_join
 from middlewares.init_contexts import InitMiddleware
 from handlers.channel.chat_member.base import new_channel
 
@@ -11,5 +12,6 @@ def create_channel_router(session_pool, bot: Bot) -> Router:
     r.my_chat_member.bind_filter(bound_filter=ChannelChatFilter)
     r.my_chat_member.middleware(InitMiddleware(session_pool, bot))
     r.my_chat_member.register(new_channel)
+    r.callback_query.register(contest_join)
 
     return r
