7eeaa541cf212cad6848cad18dd0a56d31305a1d
Roma4ka-RedBad <cool.roma2006@gmail.com>
2022-07-25 15:11:03 +0400

    Улучшение: новая клавиатура и изменение кнопок для команды start

--- app/keyboards/start.py
+++ app/keyboards/start.py
@@ -3,13 +3,34 @@ from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
+class ChannelsCallback(CallbackData, prefix="channels"):
+    action: str
+
+
 class StartCallback(CallbackData, prefix="start"):
     action: str
 
 
 def start_kb():
     kb_obj = InlineKeyboardBuilder()
-    kb_obj.row(InlineKeyboardButton(text='📝 Новости бота',
+    kb_obj.row(InlineKeyboardButton(text='💬 Каналы',
+                                    callback_data=StartCallback(action="channels").pack()),
+               InlineKeyboardButton(text='🎁 Создать Конкурс',
+                                    callback_data=StartCallback(action="contest").pack()))
+    kb_obj.row(InlineKeyboardButton(text='📋 Инструкция',
+                                    url='http://t.me/bots_TiKey'),
+               InlineKeyboardButton(text='📝 Новости бота',
                                     url='http://t.me/bots_TiKey'))
+    kb_obj.row(InlineKeyboardButton(text='🖥 Исходный код',
+                                    url='http://github.com/Like6po/for_raffle_bot'))
 
+    return kb_obj.as_markup()
+
+
+def channels_kb():
+    kb_obj = InlineKeyboardBuilder()
+    kb_obj.row(InlineKeyboardButton(text='🔗 Привязать',
+                                    callback_data=ChannelsCallback(action="add").pack()))
+    kb_obj.row(InlineKeyboardButton(text='⏪ Назад',
+                                    callback_data=ChannelsCallback(action="back").pack()))
     return kb_obj.as_markup()
