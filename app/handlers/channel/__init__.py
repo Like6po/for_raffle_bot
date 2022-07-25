d39e8c48c6268fb360453bd54497830b9e1afc99
Like6po <begemot122122@gmail.com>
2022-07-25 17:01:44 +0300

    Улучшение: рефакторинг роутеров

--- app/handlers/channel/__init__.py
+++ app/handlers/channel/__init__.py
@@ -1,18 +1,20 @@
 from aiogram import Router, Bot
 
 from filters.chat_type import ChannelChatFilter
-from handlers.channel.contest_callback.base import contest_join
-from middlewares.init_contexts import InitMiddleware
 from handlers.channel.chat_member.base import new_channel
+from handlers.channel.contest.callbacks.base import contest_join
+from middlewares.init_contexts import InitMiddleware
 
 
 def create_channel_router(session_pool, bot: Bot) -> Router:
     r: Router = Router(name="channel_router")
 
     r.my_chat_member.bind_filter(bound_filter=ChannelChatFilter)
-    r.my_chat_member.middleware(InitMiddleware(session_pool, bot))
-    r.my_chat_member.register(new_channel)
     r.callback_query.bind_filter(bound_filter=ChannelChatFilter)
+
+    r.my_chat_member.middleware(InitMiddleware(session_pool, bot))
+
+    r.my_chat_member.register(new_channel)
     r.callback_query.register(contest_join)
 
     return r
--- app/handlers/private/__init__.py
+++ app/handlers/private/__init__.py
@@ -1,34 +1,35 @@
 from aiogram import Router, Bot, F
 
 from filters.chat_type import PrivateChatFilter
+from handlers.private.addchannel.base import command_addchannel
+from handlers.private.addchannel.callbacks.channels_add import channels_add
+from handlers.private.addchannel.callbacks.channels_back import channels_back
+from handlers.private.addchannel.callbacks.channels_cancel import channels_cancel
+from handlers.private.addchannel.wait_channel import wait_channel
+from handlers.private.start.base import command_start
+from handlers.private.start.callbacks.base import start_channels
+from keyboards.channels import ChannelsCallback
+from keyboards.start import StartCallback
 from middlewares.init_contexts import InitMiddleware
-from .start.base import command_start
-from .start_callback.base import start_channels
-from .start_callback.channels import channels_back, channels_add
-from .addchannel.base import command_addchannel
-from .addchannel.state import state_addchannel
-
 from states.user import UserStatus
 
 
 def create_private_router(session_pool, bot: Bot) -> Router:
     private_router: Router = Router(name="private_router")
 
-    # Сообщения
     private_router.message.bind_filter(bound_filter=PrivateChatFilter)
-    private_router.message.middleware(InitMiddleware(session_pool, bot))
     private_router.callback_query.bind_filter(bound_filter=PrivateChatFilter)
+
+    private_router.message.middleware(InitMiddleware(session_pool, bot))
     private_router.callback_query.middleware(InitMiddleware(session_pool, bot))
 
-    private_router.message.register(command_start,
-                                    commands=["start"])
-    private_router.message.register(command_addchannel,
-                                    commands=["addchannel"], state=None)
+    private_router.message.register(command_start, commands=["start"])
+    private_router.message.register(command_addchannel, commands=["addchannel"], state=None)
+    private_router.message.register(wait_channel, state=UserStatus.wait_channel_message)
 
-    private_router.message.register(state_addchannel, state=UserStatus.wait_channel_message)
-
-    private_router.callback_query.register(start_channels, F.data == 'start:channels')
-    private_router.callback_query.register(channels_back, F.data == 'channels:back')
-    private_router.callback_query.register(channels_add, F.data == 'channels:add')
+    private_router.callback_query.register(start_channels, StartCallback.filter())
+    private_router.callback_query.register(channels_back, ChannelsCallback.filter(F.action == "back"))
+    private_router.callback_query.register(channels_add, ChannelsCallback.filter(F.action == "add"))
+    private_router.callback_query.register(channels_cancel, ChannelsCallback.filter(F.action == "cancel"))
 
     return private_router
