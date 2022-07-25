163ddcf06f5c98eb4ae7fd3685015e750541dc8f
Like6po <begemot122122@gmail.com>
2022-07-25 17:01:27 +0300

    Улучшение: рефакторинг хендлеров

---
+++ app/handlers/private/addchannel/wait_channel.py
@@ -0,0 +1,61 @@
+from aiogram import Bot
+from aiogram.dispatcher.fsm.context import FSMContext
+from aiogram.exceptions import TelegramForbiddenError
+from aiogram.types import Message
+from aiogram.utils.markdown import hbold
+
+from database.contexts.channel import ChannelContext
+from database.contexts.user import UserContext
+from database.contexts.user_channel import UserChannelContext
+from database.models.channel import Channel
+from database.models.user import User
+from keyboards.channels import channels_kb
+from misc.links import chat_link
+
+
+async def wait_channel(message: Message,
+                       state: FSMContext,
+                       bot: Bot,
+                       user_db: UserContext,
+                       channel_db: ChannelContext,
+                       user_channels_db: UserChannelContext):
+    if message.text.startswith('@'):
+        channel: Channel = await channel_db.get(await bot.get_chat(message.text))
+    elif message.forward_from_chat:
+        channel: Channel = await channel_db.get(message.forward_from_chat)
+    else:
+        await message.answer("Ожидаю либо юзернейм канала, либо пересланное с канала сообщение! Попробуйте снова!")
+        return
+
+    if not channel:
+        await message.answer("Пригласите меня в канал и выдайте мне права на публикацию сообщений в канале! "
+                             "Попробуйте снова!")
+        return
+
+    try:
+        if (await bot.get_chat_member(channel.tg_id, message.from_user.id)).status not in ['creator', 'administrator']:
+            await message.answer(f"Вы не являетесь администратором канала! Попробуйте снова!")
+            return
+
+        if not (await bot.get_chat_member(channel.tg_id, bot.id)).can_post_messages:
+            await message.answer("Выдайте мне права на публикацию сообщений в канале! Попробуйте снова!")
+            return
+
+    except TelegramForbiddenError:
+        await message.answer("Пригласите бота в канал и выдайте ему права администратора! Попробуйте снова!")
+
+    user: User = await user_db.get_or_create_and_get(message.from_user)
+    if not await user_channels_db.check_exists(user.id, channel.id):
+        await user_channels_db.new(user.id, channel.id)
+
+    await state.clear()
+
+    user_channels_list = await user_channels_db.get_all_user_channels(message.from_user)
+
+    user_channels = [f"{index + 1}. {chat_link(username=channel.username, id=channel.tg_id, title=channel.title)}"
+                     for index, channel in enumerate(user_channels_list)]
+
+    await message.answer(f"Отлично! Канал {hbold(channel.title)} успешно привязан!\n\n"
+                         f"Привязанные каналы:\n\n" + "\n".join(user_channels),
+                         disable_web_page_preview=True,
+                         reply_markup=channels_kb())
---
+++ app/handlers/private/start/callbacks/base.py
@@ -0,0 +1,20 @@
+from aiogram.types import CallbackQuery
+
+from database.contexts.user_channel import UserChannelContext
+from keyboards.channels import channels_kb
+from misc.links import chat_link
+
+
+async def start_channels(cbq: CallbackQuery,
+                         user_channels_db: UserChannelContext):
+    user_channels_list = await user_channels_db.get_all_user_channels(cbq.from_user)
+    if not user_channels_list:
+        return await cbq.message.edit_text("Вы ещё не привязали ни одного канала!",
+                                           reply_markup=channels_kb())
+
+    user_channels = [f"{index + 1}. {chat_link(username=channel.username, id=channel.tg_id, title=channel.title)}"
+                     for index, channel in enumerate(user_channels_list)]
+
+    await cbq.message.edit_text("Привязанные каналы:\n\n" + "\n".join(user_channels),
+                                disable_web_page_preview=True,
+                                reply_markup=channels_kb())
