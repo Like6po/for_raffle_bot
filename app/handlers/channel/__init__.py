a0e829afde11cd70725dce8c9e65ab11ca34ef6e
Roma4ka-RedBad <cool.roma2006@gmail.com>
2022-07-25 15:08:23 +0400

    Фикс: кнопка, которая будет в сообществе

--- app/handlers/channel/__init__.py
+++ app/handlers/channel/__init__.py
@@ -12,6 +12,7 @@ def create_channel_router(session_pool, bot: Bot) -> Router:
     r.my_chat_member.bind_filter(bound_filter=ChannelChatFilter)
     r.my_chat_member.middleware(InitMiddleware(session_pool, bot))
     r.my_chat_member.register(new_channel)
+    r.callback_query.bind_filter(bound_filter=ChannelChatFilter)
     r.callback_query.register(contest_join)
 
     return r
--- app/handlers/channel/contest_callback/base.py
+++ app/handlers/channel/contest_callback/base.py
@@ -3,6 +3,5 @@ from aiogram.types import CallbackQuery
 from keyboards.contest import ContestCallback
 
 
-async def contest_join(cbq: CallbackQuery,
-                       callback_data: ContestCallback):
+async def contest_join(cbq: CallbackQuery):
     print(f"Получено нажатие на кнопку участия в конкурсе в канале {cbq.message.chat.title}!")
