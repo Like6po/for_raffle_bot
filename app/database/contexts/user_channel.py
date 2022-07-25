b3d499536925b607bd340286865d1964b3a09464
Like6po <begemot122122@gmail.com>
2022-07-25 17:00:27 +0300

    Улучшение: изменил контекст UserChannel

--- app/database/contexts/user_channel.py
+++ app/database/contexts/user_channel.py
@@ -1,14 +1,15 @@
 import logging
-from typing import Any, Union, Type
+from typing import Union, Type, cast
 
 from aiogram.types.user import User as AiogramUser
-from sqlalchemy.exc import IntegrityError, DataError
-from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select, exists
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.asyncio import AsyncSession, AsyncResult
 from sqlalchemy.orm import sessionmaker
 
 from database.contexts.base import DatabaseContext, SQLAlchemyModel
-from database.models.user import User
 from database.models.channel import Channel
+from database.models.user import User
 from database.models.user_channel import UserChannel
 
 
@@ -21,30 +22,25 @@ class UserChannelContext(DatabaseContext):
             query_model: Type[SQLAlchemyModel]):
         super().__init__(session_or_pool, query_model=query_model)
 
-    async def get(self, user: User | None = None,
-                  channel: Channel | None = None):
-        if user and channel:
-            return await super().get_one(UserChannel.user_id == user.id, UserChannel.channel_id == channel.id)
-        elif user:
-            return await super().get_all(UserChannel.user_id == user.id)
-        elif channel:
-            return await super().get_all(UserChannel.channel_id == channel.id)
-        raise DataError
-
-    async def new(self, user: User | None = None,
-                  channel: Channel | None = None,
-                  **values: Any) -> UserChannel:
+    async def new(self, user_id: int, channel_id: int) -> UserChannel:
         try:
-            if user and channel:
-                return await super().add(user_id=user.id,
-                                         channel_id=channel.id,
-                                         **values)
-            raise DataError
-
+            return await super().add(user_id=user_id,
+                                     channel_id=channel_id)
         except IntegrityError as e:
             logging.exception("Не смог вставить в базу...", exc_info=e)
 
-    async def check_and_create(self, user: User | None = None,
-                               channel: Channel | None = None):
-        if not await self.get(user, channel):
-            return await self.new(user, channel)
+    async def check_exists(self, user_id: int, channel_id: int):
+        statement = exists(self.model).where(UserChannel.user_id == user_id,
+                                             UserChannel.channel_id == channel_id).select()
+        async with self._transaction:
+            result: AsyncResult = await self._session.execute(statement)
+            return cast(bool, result.scalar())
+
+    async def get_all_user_channels(self, user: AiogramUser | None = None):
+        statement = select(UserChannel.channel_id, Channel.title, Channel.tg_id, Channel.username). \
+            join(Channel, Channel.id == UserChannel.channel_id). \
+            join(User, User.id == UserChannel.user_id). \
+            where(User.tg_id == user.id)
+        async with self._transaction:
+            result: AsyncResult = await self._session.execute(statement)
+            return result.all()
