89b517654ffd18c2dca627766e7905b9097d74d7
Roma4ka-RedBad <cool.roma2006@gmail.com>
2022-07-25 15:24:37 +0400

    Улучшение: добавил возможность поиска канала в бд по его id

--- app/database/contexts/channel.py
+++ app/database/contexts/channel.py
@@ -20,11 +20,13 @@ class ChannelContext(DatabaseContext):
         super().__init__(session_or_pool, query_model=query_model)
 
     async def get(self, chat: AiogramChat | None = None,
-                  tg_id: int | None = None):
+                  tg_id: int | None = None, channel_id: int | None = None):
         if chat:
             return await super().get_one(Channel.tg_id == chat.id)
         elif tg_id:
             return await super().get_one(Channel.tg_id == tg_id)
+        elif channel_id:
+            return await super().get_one(Channel.id == channel_id)
         raise DataError
 
     async def new(self, chat: AiogramChat | None = None,
