67941b138527cd84e3e016d749c924f738026d7d
Roma4ka-RedBad <cool.roma2006@gmail.com>
2022-07-31 16:53:17 +0400

    Улучшение: вспомогательная функция для распределения массива каналов на страницы для кнопок.

---
+++ app/misc/pages.py
@@ -0,0 +1,11 @@
+def array_to_pages(array: list = []) -> dict:
+    pages = {
+        0: []
+    }
+    for element in array:
+        last_page = list(pages)[-1]
+        if len(pages[last_page]) == 5:
+            pages[last_page + 1] = [element]
+        else:
+            pages[last_page].append(element)
+    return pages
