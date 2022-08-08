893cf579991530d89de3c7c82b2c7299241464b0
Roma4ka-RedBad <cool.roma2006@gmail.com>
2022-08-08 22:54:48 +0400

    Улучшение: создал статус конкурса

---
+++ app/states/contest.py
@@ -0,0 +1,13 @@
+from aiogram.dispatcher.fsm.state import State, StatesGroup
+
+
+class ContestStatus(StatesGroup):
+    channel_id = State()
+    text = State()
+    btn_title = State()
+    attachment_hash = State()
+    is_attachment_preview = State()
+    winner_count = State()
+    start_at = State()
+    end_at = State()
+    end_count = State()
