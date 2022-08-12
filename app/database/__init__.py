200c2a0887c0883d2a38219489da67ed84c2a51d
Daniel <49990444+Dan1ello@users.noreply.github.com>
2022-08-12 13:48:08 +0300

    Фикс: циркулярный импорт

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,15 +1,16 @@
-from .contexts.base import DatabaseContext
-from .contexts.channel import ChannelContext
-from .contexts.contest import ContestContext
-from .contexts.member import MemberContext
-from .contexts.user import UserContext
-from .contexts.user_channel import UserChannelContext
 from .models.base import DatabaseModel
 from .models.channel import Channel
 from .models.contest import Contest
 from .models.member import Member
 from .models.user import User
 from .models.user_channel import UserChannel
+
+from .contexts.base import DatabaseContext
+from .contexts.channel import ChannelContext
+from .contexts.contest import ContestContext
+from .contexts.member import MemberContext
+from .contexts.user import UserContext
+from .contexts.user_channel import UserChannelContext
 
 __all__ = (
     # Models
