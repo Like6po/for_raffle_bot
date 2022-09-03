fa15bcfef2dd7f71808e4f9bf05c82b9c312e523
Roma4ka-RedBad <cool.roma2006@gmail.com>
2022-09-03 21:33:53 +0400

    Улучшение: проверка на наличие каналов спонсоров для устранения ошибки, изменения текста

--- app/handlers/channel/contest/callbacks/base.py
+++ app/handlers/channel/contest/callbacks/base.py
@@ -18,13 +18,14 @@ async def contest_join(cbq: CallbackQuery,
     member_data = await member_db.get_or_create_and_get(cbq.from_user)
     contest_data = await contest_db.get_by_db_id(callback_data.contest_db_id)
 
-    if not is_channel_member(await bot.get_chat_member(contest_data.channel_tg_id, cbq.from_user.id)):
-        return await cbq.answer('Для начала подпишитесь на этот канал!', show_alert=True)
+    if contest_data.sponsor_channels:
+        if not is_channel_member(await bot.get_chat_member(contest_data.channel_tg_id, cbq.from_user.id)):
+            return await cbq.answer('Для начала подпишитесь на этот канал!', show_alert=True)
 
-    for channel_id in contest_data.sponsor_channels:
-        if not is_channel_member(await bot.get_chat_member(channel_id, cbq.from_user.id)):
-            return await cbq.answer(f'Вы не подписаны на '
-                                    f'{html.quote((await channel_db.get(tg_id=channel_id)).title)}!', show_alert=True)
+        for channel_id in contest_data.sponsor_channels:
+            if not is_channel_member(await bot.get_chat_member(channel_id, cbq.from_user.id)):
+                return await cbq.answer(f'Вы не подписаны на '
+                                        f'{html.quote((await channel_db.get(tg_id=channel_id)).title)}!', show_alert=True)
 
     if not await contest_members_db.exists(callback_data.contest_db_id, member_data.id):
         return await cbq.answer(url=await create_start_link(bot=bot,
@@ -32,4 +33,4 @@ async def contest_join(cbq: CallbackQuery,
                                                             encode=True))
     else:
         await contest_members_db.delete(callback_data.contest_db_id, member_data.id)
-        return await cbq.answer('Вы успешно самоудалились из конкурса, молодцы.', show_alert=True)
+        return await cbq.answer('Вы больше не учавствуете в конкурсе!', show_alert=True)
