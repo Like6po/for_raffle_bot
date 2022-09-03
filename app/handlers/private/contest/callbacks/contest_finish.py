d048ef0a95245334b2acc00bcad8c744a516fab7
Roma4ka-RedBad <cool.roma2006@gmail.com>
2022-09-03 21:38:27 +0400

    Улучшение: перенёс всё из функции выбора победителей в функцию каллбека

--- app/handlers/private/contest/callbacks/contest_finish.py
+++ app/handlers/private/contest/callbacks/contest_finish.py
@@ -17,7 +17,29 @@ async def contest_finish_cbq(cbq: CallbackQuery,
                              channel_db: ChannelContext,
                              contest_members_db: ContestMemberContext,
                              member_db: MemberContext):
-    await choose_the_winners(bot, contest_db, contest_members_db, member_db, callback_data.contest_db_id)
+    winners_list = await choose_the_winners(contest_db, contest_members_db, member_db, callback_data.contest_db_id)
+
+    if len(winners_list) <= 0:
+        return await cbq.message.edit_text('Не могу выбрать победителя! Никто не учавствует!',
+                                           reply_markup=results_kb(
+                                               await contest_db.get_all(callback_data.channel_db_id),
+                                               callback_data.channel_db_id))
+
+    string = f'Конкурс завершён! Спасибо за участие!\n\nСписок победителей:\n' + \
+             '\n'.join(f'{i + 1}) {user_link(title=user.full_name, tg_id=user.tg_id)}'
+                       for i, user in enumerate(winners_list))
+
+    msg = await bot.send_message(contest_data.channel_tg_id, string, reply_to_message_id=contest_data.message_id)
+    link_to_post = post_link(contest_data.channel_tg_id, msg.message_id)
+
+    if contest_data.attachment_hash:
+        await bot.edit_message_caption(contest_data.channel_tg_id,
+                                       contest_data.message_id,
+                                       caption=contest_data.text + f'\n\nПобедители: {link_to_post}')
+    else:
+        await bot.edit_message_text(contest_data.text + f'\n\nПобедители: {link_to_post}',
+                                    contest_data.channel_tg_id,
+                                    contest_data.message_id)
 
     await cbq.message.edit_text(
         make_list_of_current_contests_text(await channel_db.get(channel_id=callback_data.channel_db_id),
